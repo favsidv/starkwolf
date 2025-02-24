e43a4403a6544297653ed7eaab6bcfdc57bfdb6c
KyllianGenot <kygenot@gmail.com>
2025-02-24 13:56:17 -0700

    Push frontend

--- client/tests/bot.ts
+++ client/tests/bot.ts
@@ -1,11 +1,13 @@
 import { RpcProvider, Account, Contract, shortString, CallData } from 'starknet';
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import { promisify } from 'util';
+import fs from 'fs';
 import dotenv from 'dotenv';
 
 // Charger les variables d'environnement
 dotenv.config();
 
-// Importer l'ABI directement (adapté pour Vite)
-import abiData from './abi_actions.json';
 /**
  * Type for a Starknet contract address (hex string starting with 0x)
  */
@@ -15,6 +17,14 @@ type ContractAddress = string;
  * Type for a Starknet account address (hex string starting with 0x)
  */
 type AccountAddress = string;
+
+// Définition du type pour le service Torii
+type ToriiService = {
+    Torii: {
+        service: grpc.ServiceDefinition<any>;
+        [key: string]: any;
+    }
+};
 
 /**
  * Main class to interact with the StarkWolf game contract
@@ -34,12 +44,33 @@ export class StarkWolfGame {
     constructor(
         contractAddress: ContractAddress,
         nodeUrl: string = 'https://api.cartridge.gg/x/starkwolf/katana',
+        grpcUrl: string = 'http://localhost:8080'
     ) {
         this.provider = new RpcProvider({ nodeUrl });
         this.accounts = new Map();
         
-        // Utiliser l'ABI importé directement
-        this.contract = new Contract(abiData.abi, contractAddress, this.provider);
+        // Load ABI from JSON file
+        const abiPath = './abi_actions.json';
+        const abiData = fs.readFileSync(abiPath, 'utf-8');
+        const abi = JSON.parse(abiData).abi;
+        this.contract = new Contract(abi, contractAddress, this.provider);
+
+        // Configuration gRPC
+        const packageDefinition = protoLoader.loadSync('./torii.proto', {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true
+        });
+
+        const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+        const toriiService = (protoDescriptor.torii as ToriiService);
+        
+        this.grpcClient = new (toriiService.Torii as any)(
+            grpcUrl,
+            grpc.credentials.createInsecure()
+        );
     }
 
     addAccount(address: string, privateKey: string) {
